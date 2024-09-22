bc48a74d4d44b0a74ccf4bcbc6feca7b55f07cf4
Hasindu19 <89360985+Hasindu19@users.noreply.github.com>
2024-09-23 02:11:14 +1000

    feat: added get reminder time in milliseconds

--- js/scriptOne.js
+++ js/scriptOne.js
@@ -75,3 +75,21 @@ function saveTasks() {
     // Save tasks to localStorage
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+// Function to get reminder time in milliseconds
+function getReminderTime(reminder) {
+    switch (reminder) {
+        case '15m':
+            return 15 * 60 * 1000;
+        case '30m':
+            return 30 * 60 * 1000;
+        case '1h':
+            return 60 * 60 * 1000;
+        case '2h':
+            return 2 * 60 * 60 * 1000;
+        case '1d':
+            return 24 * 60 * 60 * 1000;
+        default:
+            return 0;
+    }
+}
